65d78cbf977c76d785cfafa8cbf36eac0c7e4362
nejishow <ashrafmuhamad2014@gmail.com>
2022-07-27 06:07:54 +0300

    add name

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,44 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    goodAnswers: [],
+    myAnswers: [],
+    name: ''
+  },
+  getters: {
+
   },
   mutations: {
+    addNewAnswer(state, { indexArray, answerIndex }) {
+      state.myAnswers[indexArray] = answerIndex
+      localStorage.setItem('myAnswers', state.myAnswers)
+    },
+    loadAnswers(state) {
+      state.myAnswers = localStorage.getItem('myAnswers')
+    },
+    registerName(state, name) {
+      state.name = name
+      state.name = localStorage.setItem('name', name)
+    },
+    loadName(state) {
+      state.name = localStorage.getItem('name')
+    }
   },
   actions: {
+    ADD_NEW({ commit }, { indexArray, answerIndex }) {
+      commit('addNewAnswer', { indexArray, answerIndex });
+
+    },
+    LOAD({ commit }) {
+      commit('addNewAnswer');
+    },
+    REGISTER({ commit }, name) {
+      commit('registerName', name);
+
+    },
+    LOADNAME({ commit }) {
+      commit('loadName');
+    }
   },
   modules: {
   }
---
+++ src/views/Questions.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Pomodoro theme="dark" />
+  </div>
+</template>
+<script>
+import Pomodoro from "vuemodoro";
+
+export default {
+  components: {
+    Pomodoro,
+  },
+};
+</script>
